d9060dd3ca31e2e2cb2815a0ba7f144edb5e0e34
Terry Shi <weiceica@umich.edu>
2022-12-25 16:16:53 -0800

    sort dataset

--- streaming/main.py
+++ streaming/main.py
@@ -15,12 +15,50 @@ def getMyStreaming(path: str = '../streaming') -> List[dict]:
     
     return allSongs
 
-#obtain features information
+#obtain features information    
+def songFeatures(myStreamings) -> List[dict]:
+    #TODO:
+    return 1 
 
 #convert features information into Json File
+def songToJSON(myStreamings) -> List[dict]:
+    #TODO:
+    return 1
 
 #get amount of times I listened to each song
+def songAmount(myStreamings): # return type is a dictionary of dictionaries
+    dic = {}
+    for streaming in myStreamings:
+        if streaming['trackName'] not in dic:
+            dic[streaming['trackName']] = {}
+            dic[streaming['trackName']]['timesPlayed'] = 1
+            dic[streaming['trackName']]['timePlayed'] = streaming['msPlayed']
+        else:
+            dic[streaming['trackName']]['timesPlayed'] += 1
+            dic[streaming['trackName']]['timePlayed'] += streaming['msPlayed']
+    return dic
 
+#sort streaming by time played
+def sortTimePlayed(songDict): # 
+    sortDict = sorted(songDict.items(), key=lambda x:x[1]['timePlayed'], reverse=True)
+    convertDict = dict(sortDict)
+    return convertDict
+    
+    
+# sort streaming by times played   
+def sortTimesPlayed(songDict):
+    #TODO:
+    sortDict = sorted(songDict.items(), key=lambda x:x[1]['timesPlayed'], reverse=True)
+    convertDict = dict(sortDict)
+    return convertDict
+
+def wrapped():
+    #TODO
+    return 1
+
+     
 if __name__ == '__main__':
-    myStreaming = getMyStreaming()
-    print(myStreaming)
+    myStreaming = getMyStreaming() # creating a myStreaming list of dictionaries
+    songDict = songAmount(myStreaming) # creating a dong dictionary (dict of dicts)
+    sortTime = sortTimePlayed(songDict)
+    sortTimes = sortTimesPlayed(songDict)
